ccc5d8b0c478a5ff6725efcb5675632a643303b7
muxtarofficiall <161587396+muxtarofficiall@users.noreply.github.com>
2024-03-26 05:55:03 -0700

    Create app1.js

---
+++ app1/app1.js
@@ -0,0 +1,25 @@
+##task1 İstifadəçinin daxil etdiyi adın neçə simvoldan ibarət olduğunu tapın.##
+  //let name = prompt("adinizi daxil edin");
+ //console.log(name.length);
+##task2 İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvol varmı .##
+//let soz=prompt("bir sozu daxil edin");
+//let simvol= prompt("bir simvol girin ")
+//console.log(soz.includes(simvol));
+##task4 .İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla bitirmi##
+// let soz=prompt("bir sozu daxil edin");
+// let simvol= prompt("bir simvol girin ")
+// console.log(soz.endsWith(simvol));
+
+##task5 .İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvolun index ini tapın##
+// let soz=prompt("bir sozu daxil edin");
+// let simvol= prompt("bir simvol girin ");
+//  console.log(soz.indexOf(simvol));
+
+## task 3İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla başlayırmı ##
+//let soz=prompt("bir sozu daxil edin");
+ //let simvol= prompt("bir simvol girin ")
+ //console.log(soz.startsWith(simvol));
+task6 ##İstifadəçinin daxil etdiyi sözün böyük hərf olub olmadığını tapın.##
+// const isUpperCase = str => str === str.toUpperCase();
+// let soz=prompt("bir sozu daxil edin");
+// console.log(isUpperCase(soz));
